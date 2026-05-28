83731c0e1d8bdeb73c5b2b469db8d8cc939fcbce
Matth-kaiton <matthias.dumas@ynov.com>
2026-05-28 14:19:54 +0200

    ajoue du docker compose pour le front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +13,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 3000,
+    host: true, 
+    watch: {
+      usePolling: true,
     },
   },
 })
