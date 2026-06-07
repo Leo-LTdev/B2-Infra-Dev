52fd7e102626fc6e9692ae59883c2c9505cdf0fb
Matth-kaiton <matthias.dumas.pro@gmail.com>
2026-06-07 19:18:26 +0200

    fix

--- back/server/src/routes/bienRoutes.js
+++ back/server/src/routes/bienRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const  bienController = require('../controllers/bienController');
+const bienController = require('../controllers/bienController');
+const  { upload } = require('../controllers/bienController');
 
 // Routes pour les biens immobiliers
-router.post('/', bienController.createBien);
+router.post('/', upload.single('image'), bienController.createBien);
 router.get('/', bienController.getAllBiens);
 // router.get('/:id', bienController.getBienById);
 // router.put('/:id', bienController.updateBien);
